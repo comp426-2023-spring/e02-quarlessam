19d32e284b7aff17cd15c1f9f7e589ac2d66e78a
quarlessam <quarlessam@gmail.com>
2023-05-09 01:46:07 -0400

    initial commit

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,40 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+async function rps_fetch(endpoint) {
+	let element = document.getElementById("button")
+	var response = await fetch(window.location.href + endpoint)
+	console.log(response)
+	var json = await response.json()
+}
+
+function rps_button() {
+	let element = document.getElementById("endpoint")
+	element.textContent = element.textContent.trim() + "/rps"
+	document.getElementById("rps_button").setAttribute("disabled", "disabled")
+	document.getElementById("rpsls_button").setAttribute("disabled", "disabled")
+	document.getElementById("opp_question").removeAttribute("hidden")
+	document.getElementById("opp_button").removeAttribute("hidden")
+	document.getElementById("no_opp_button").removeAttribute("hidden")
+}
+function rpsls_button() {
+	let element = document.getElementById("endpoint")
+	element.textContent = element.textContent.trim() + "/rpsls"
+	document.getElementById("opp_question").removeAttribute("hidden")
+	document.getElementById("rps_button").setAttribute("disabled", "disabled")
+	document.getElementById("rpsls_button").setAttribute("disabled", "disabled")
+}
+function opp_button() {
+	let element = document.getElementById("endpoint")
+	element.textContent = element.textContent.trim() + "/rpsls"
+	document.getElementById("opp_question").removeAttribute("hidden")
+	document.getElementById("opp_button").setAttribute("disabled", "disabled")
+	document.getElementById("no_opp_button").setAttribute("disabled", "disabled")
+}
+function no_opp_button() {
+	let element = document.getElementById("endpoint")
+	element.textContent = element.textContent.trim() + "/rpsls"
+	document.getElementById("opp_question").removeAttribute("hidden")
+	document.getElementById("opp_button").setAttribute("disabled", "disabled")
+	document.getElementById("no_opp_button").setAttribute("disabled", "disabled")
+}
--- server.js
+++ server.js
@@ -43,6 +43,11 @@ import express from 'express'
 // https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
 import path from 'path'
 import { fileURLToPath } from 'url'
+
+// AO4 IMPORTS //
+import {rps} from './lib/rpsls.js'
+import {rpsls} from './lib/rpsls.js'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 // Load dependencies for logging
@@ -71,6 +76,50 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+// A04 API FUNCTIONS //
+app.get('/app', function(req, res){
+  res.send("200 OK")
+})
+
+app.get('/app/rps', function(req, res){
+  res.send(rps())
+})
+
+app.get('/app/rpsls', function(req, res){
+  res.send(rpsls())
+})
+
+app.get('/app/rps/play', function(req, res){
+  res.send(rps(req.query.shot))
+})
+
+app.post('/app/rps/play', function(req, res){
+  res.send(rps(req.body.shot))
+})
+
+app.get('/app/rps/play/:shot', function(req, res){
+  res.send(rps(req.params.shot))
+})
+
+
+app.get('/app/rpsls/play', function(req, res){
+  res.send(rpsls(req.query.shot))
+})
+
+app.post('/app/rpsls/play', function(req, res){
+  res.send(rpsls(req.body.shot))
+})
+
+app.get('/app/rpsls/play/:shot', function(req, res){
+  res.send(rpsls(req.params.shot))
+})
+
+app.get("*", (req, res) => {
+  res.send("404 NOT FOUND")
+})
+// A04 API FUNCTIONS - END //
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
